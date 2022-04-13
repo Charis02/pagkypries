6925a89b0ca75daa56afcec2c546ad560a02ba1a
Charis02 <chge2002@gmail.com>
2022-04-13 00:38:30 -0400

    Fix "back to top" button for safari using jquery

--- javascript/scroll_to_top.js
+++ javascript/scroll_to_top.js
@@ -1,20 +1,18 @@
-btn = document.getElementById("top-button");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    btn.style.display = "flex";
-    value = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
-    btn.style.opacity = Math.min(1,(value)/10000);
-  } else {
-    btn.style.display = "none";
-    btn.style.opacity = 0;
-  }
-}
+$(document).ready(function(){
+    let btn = $("#top-button");
+    $(window).scroll(function(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.css('display', 'flex');
+        value = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+      btn.css('opacity',Math.min(1,(value)/5000));
+    } else {
+        btn.css('display', 'none');
+        btn.css('opacity',0);
+    }});
 
 
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+    btn.click( function() {
+        console.log("here");
+        $(document).scrollTop(0);
+    });
+});
