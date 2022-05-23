45991d5c50eb1e447edce649c38882bb390d92ae
Charis02 <chge2002@gmail.com>
2022-05-23 08:40:07 +0300

    add page initialization handler

---
+++ javascript/initialize_pages.js
@@ -0,0 +1,28 @@
+function initialize_page()
+{
+    let page_name = window.location.pathname.split("/").pop();
+    switch(page_name)
+    {
+        case "mean_rank":
+            mean();
+            break;
+        case "minmax":
+            minmax();
+            break;
+        case "rank_by_lesson":
+            rank_by_lesson();
+            break;
+        case "cyprus_curved":
+            cyprus_curved();
+            break;
+        case "bases":
+            bases();
+            break;
+        default:
+            break;
+    }
+
+    return;
+}
+
+initialize_page();
