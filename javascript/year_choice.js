9f2a06150696f27fac13e261a855a8654c8f42f7
Charis02 <chge2002@gmail.com>
2022-05-23 08:42:01 +0300

    handle change of year

---
+++ javascript/year_choice.js
@@ -0,0 +1,30 @@
+let indicator = document.getElementById("year_indicator");
+let DEFAULT_YEAR=2020;
+let MIN_YEAR = 2019;
+let MAX_YEAR = 2022;
+let YEAR_DIFF = MAX_YEAR - MIN_YEAR+1;
+
+function set_year(year)
+{
+    localStorage.setItem('chosen_year', year);
+
+    $('.year_div').not('.'+String(year)).removeClass('is_active');
+    $('.'+String(year)).addClass('is_active');
+
+    let pos = (year - MIN_YEAR) / YEAR_DIFF*100 + '%';
+    let wid = 100/YEAR_DIFF + '%';
+
+    indicator.style.left = pos;
+    indicator.style.width = wid;
+
+    initialize_page();
+}
+
+
+$(document).ready(function() 
+{
+    if(localStorage.getItem('chosen_year')==null)
+        set_year(DEFAULT_YEAR);
+    else
+        set_year(localStorage.getItem('chosen_year'));
+});
