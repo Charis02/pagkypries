c9167e477737466f6918a79a44b672674b502c32
Charis02 <chge2002@gmail.com>
2022-05-26 16:00:59 +0300

    style forum

--- javascript/forum.js
+++ javascript/forum.js
@@ -1,86 +1,99 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
-import { getFirestore,collection, addDoc,getDoc,orderBy,startAt,limit,query,where,getDocs } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
+import { getFirestore,collection, addDoc,getDoc,doc,getDocs,query,orderBy } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js";
 
 
- const firebaseConfig = {
-    apiKey: "AIzaSyDVCPmNRgwARnbmvbBJcyCzzT5o_qqKb_o",
-    authDomain: "pagkypries-forum.firebaseapp.com",
-    projectId: "pagkypries-forum",
-    storageBucket: "pagkypries-forum.appspot.com",
-    messagingSenderId: "960261449242",
-    appId: "1:960261449242:web:e2e417db6e1f50863c93a3",
-    measurementId: "G-S38B9HK551"
-  };
+const firebaseConfig = {
+apiKey: "AIzaSyDVCPmNRgwARnbmvbBJcyCzzT5o_qqKb_o",
+authDomain: "pagkypries-forum.firebaseapp.com",
+projectId: "pagkypries-forum",
+storageBucket: "pagkypries-forum.appspot.com",
+messagingSenderId: "960261449242",
+appId: "1:960261449242:web:e2e417db6e1f50863c93a3",
+measurementId: "G-S38B9HK551"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-  const db = getFirestore(app);
-  
- $("#forum-post-submit").click(function(){
-     let title = $('#forum-post-title').val();
-     let body = $('#forum-post-body').val();
+const db = getFirestore(app);
 
-     if (title == "" || body == ""){
-         alert("Please fill in all fields!");
-         return;
-     }
 
-    let data = {
-        title: title,
-        body: body,
-        date: new Date().toLocaleString(),
-    }
-
-    addDoc(collection(db, "posts"), data);
- });
-
-async function nextHandler(pageIndex)
+function createPostElement(post,id)
 {
-    const posts = collection(db, "posts");
-    
-    const q = query(posts, where("date", "!=", 'a'));
+    let title = post.title;
+    let date = post.date;
 
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-        let frag = document.createDocumentFragment();
+    let row = document.createElement("div");
+    row.classList.add("post-card");
+    row.id = id;
+    row.onclick = getPost;
 
-        let row = document.createElement("tr");
-        row.classList.add("row");
+    let col = document.createElement("div");
+    col.classList.add("post-card-title");
+    col.innerHTML = title;
 
-        let col = document.createElement("td");
-        col.classList.add("col0");
-        col.innerHTML = doc.data().title;
+    let col2 = document.createElement("div");
+    col2.classList.add("post-card-date");
+    col2.innerHTML = date;
 
-        let col2 = document.createElement("td");
-        col2.classList.add("col1");
-        col2.innerHTML = doc.data().body;
+    row.appendChild(col);
+    row.appendChild(col2);
 
-        let col3 = document.createElement("td");
-        col3.classList.add("col2");
-        col3.innerHTML = doc.data().date;
-
-        row.appendChild(col);
-        row.appendChild(col2);
-        row.appendChild(col3);
-
-        frag.appendChild(row);
-
-        return this.append(Array.from(frag.childNodes))
-          // indicate that there is a next page to load
-          .then(() => false);
-    });
-
-    return false;
+    return row;
 }
 
-window.ias = new InfiniteAjaxScroll('#table-body', {
-item: '.row',
-next: nextHandler,
-pagination: false,
-negativeMargin: 400,
-prefill: true,
+async function refreshPosts(){
+    $('#forum-posts-overflow').empty();
+
+    const q = query(collection(db, "posts"), orderBy("date", "desc"));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+        let post = doc.data();
+        let row = createPostElement(post,doc.id);
+        $("#forum-posts-overflow").append(row);
+    });
+}
+
+async function getPost(){
+    let id = $(this).attr('id');
+
+    const docRef = doc(db, "posts", id);
+    const docSnap = await getDoc(docRef);
+
+    let post = docSnap.data();
+    let title = post.title;
+    let body = post.body;
+
+    $('#chosen-post-title').html('<h3>'+title+'</h3>');
+    $('#chosen-post-body').html(body);
+}
+
+$(document).ready(function(){
+    refreshPosts();
+});
+
+$("#forum-post-submit").click(function(){
+    let title = $('#forum-post-title').val();
+    let body = $('#forum-post-body').val();
+
+    if (title == "" || body == ""){
+        alert("Please fill in all fields!");
+        return;
+    }
+
+   let data = {
+       title: title,
+       body: body,
+       date: new Date().toLocaleString(),
+   }
+
+   addDoc(collection(db, "posts"), data);
+
+    $('#forum-post-title').val("");
+    $('#forum-post-body').val("");
+
+    refreshPosts();
 });
